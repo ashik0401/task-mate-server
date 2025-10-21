8bf3138ea0cf775e52c5030f8ea70d8322cd452d
ashik0401 <ashik04am@gmail.com>
2025-10-21 20:26:02 +0600

    added some code for backend run

--- index.js
+++ index.js
@@ -66,5 +66,10 @@ app.get("/tasks", async (req, res) => {
   res.json(tasks);
 });
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
